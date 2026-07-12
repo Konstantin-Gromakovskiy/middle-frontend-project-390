f955e7aecfa43d7c0e4e15bf7598cce87902aa0d
konstantin <k.hromakovskiy@yandex.ru>
2026-07-12 18:18:44 +0300

    добавил автокомплит по городам

--- code/src/features/search-flights/ui/FlightSearchForm.tsx
+++ code/src/features/search-flights/ui/FlightSearchForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button, Flex, NumberInput, TextInput } from '@mantine/core'
+import { Autocomplete, Button, Flex, NumberInput } from '@mantine/core'
 import { DateInput } from '@mantine/dates'
 import { useForm } from '@mantine/form'
 import type { City } from '@/shered/api/server-api.types'
@@ -17,7 +17,7 @@ type FlightSearchFormProps = {
   searchAction: (values: FlightSearchValues) => void
 }
 
-export function FlightSearchForm({ searchAction }: FlightSearchFormProps) {
+export function FlightSearchForm({ cities, searchAction }: FlightSearchFormProps) {
   const form = useForm<FlightSearchValues>({
     initialValues: {
       origin: '',
@@ -31,11 +31,26 @@ export function FlightSearchForm({ searchAction }: FlightSearchFormProps) {
     searchAction(values)
   })
 
+  const cityOptions = cities.map(({ code, name, country }) => ({
+    value: name,
+    label: `${name}${country ? `, ${country}` : ''} (${code})`,
+  }))
+
   return (
     <form onSubmit={submitFrom}>
       <Flex gap="lg" align="flex-end">
-        <TextInput label="Откуда" placeholder="Город вылета" {...form.getInputProps('origin')} />
-        <TextInput label="Куда" placeholder="Город прилёта" {...form.getInputProps('destination')} />
+        <Autocomplete
+          label="Откуда"
+          placeholder="Город вылета"
+          data={cityOptions}
+          {...form.getInputProps('origin')}
+        />
+        <Autocomplete
+          label="Куда"
+          placeholder="Город прилёта"
+          data={cityOptions}
+          {...form.getInputProps('destination')}
+        />
         <DateInput
           label="Дата"
           placeholder="Выберите дату"
