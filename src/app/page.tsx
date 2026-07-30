a08ed17cf0b042225e10a6f3be134f53129f43ea
konstantin <k.hromakovskiy@yandex.ru>
2026-07-30 23:27:13 +0300

    Добавил заголовок

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Container } from '@mantine/core'
+import { Container, Title } from '@mantine/core'
 import { FlightResults } from '@/widgets/flight-results/ui/FlightResults'
 
 export default function Home() {
   return (
     <Container component="main">
+      <Title order={1}>Поиск авиабилетов</Title>
       <FlightResults />
     </Container>
   )
